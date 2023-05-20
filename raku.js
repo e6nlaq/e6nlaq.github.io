ff2ab872bf79cea8a298d5f83db32438001469e8
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-05-20 09:10:32 +0900

    NEO

--- raku.js
+++ raku.js
@@ -51,6 +51,6 @@ window.onload = function () {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em }";
     document.body.appendChild(css);
 };
